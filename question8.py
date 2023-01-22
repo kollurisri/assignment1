a98d3c072c5c020d31e1267cac0b0329371bffdf
kollurisri <122470948+kollurisri@users.noreply.github.com>
2023-01-23 00:04:02 +0530

    Area and radius of the circle

---
+++ question8.py
@@ -0,0 +1,4 @@
+Radius = 10
+print("Radius = {} ".format(Radius))
+print("Area = {} * Radius**2".format(3.14))
+print("The area of a circle with radius {} is {} meters square.".format(10,int(3.14*10**2)))
